4ff70836fb3d876cb2cdd6a8d61d31773af87e90
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-08 08:45:30 +0300

    add navigator

---
+++ lib/views/home_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/views/search_view.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Weather App"),
+        backgroundColor: Colors.blue,
+        actions: [
+          IconButton(onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder:(context){
+              return SearchView();
+            },),);
+          },icon: const Icon(Icons.search),)
+
+        ],
+
+      ),
+    );
+  }
+}
