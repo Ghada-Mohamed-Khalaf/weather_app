198e10ea6f29f49518edff6e19bc7c916b72ef87
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-11 12:28:11 +0200

    create weather_model

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,12 @@
+class WeatherModel{
+  final String cityName;
+  final String date;
+  final  String image;
+  final String maxTemp;
+  final String minTemp;
+  final String temp;
+  final String weatherCondition;
+
+  WeatherModel({required this.cityName, required this.date, required this.image, required this.maxTemp, required this.minTemp, required this.temp, required this.weatherCondition});
+
+}
