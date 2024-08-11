524c66ee26a26e22147d7766123758c519ba7ad7
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-11 20:56:51 +0200

    add dio package

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,4 +1,12 @@
+import 'package:weather_app/models/weather_model.dart';
+
 class WeatherServices{
+     final Dio dio = Dio();
+
+     WeatherModel getCurrentWeather(){
+          Dio().get(path)
+
+     }
 
 
 }
