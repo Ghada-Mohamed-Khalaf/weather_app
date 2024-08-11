8bb9525b38fcccb34cd1d773efc5c85b3481debb
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-11 21:50:12 +0200

    get current weatherModel

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,10 +1,16 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:weather_app/models/weather_model.dart';
 
 class WeatherServices{
      final Dio dio = Dio();
 
-     WeatherModel getCurrentWeather(){
-          Dio().get(path)
+     Future< WeatherModel> getCurrentWeather({required String cityName}) async{
+    Response response=await dio.get('https://api.weatherapi.com/v1/forecast.json?key=68e2c2078b044de3b3672943241108&q=$cityName&days=1');
+    WeatherModel weatherModel=WeatherModel.fromJson(response.data);
+    return weatherModel;
 
      }
 
