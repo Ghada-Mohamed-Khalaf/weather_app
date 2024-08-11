a6bd38b8ba228842ad4b5a1f5b9a6342af4b64ba
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-11 16:25:15 +0200

    create constructor and factory  in weatherModel

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,12 +1,23 @@
-class WeatherModel{
+class WeatherModel {
   final String cityName;
   final String date;
-  final  String image;
+  final String? image;
   final String maxTemp;
   final String minTemp;
   final String temp;
   final String weatherCondition;
 
-  WeatherModel({required this.cityName, required this.date, required this.image, required this.maxTemp, required this.minTemp, required this.temp, required this.weatherCondition});
+  WeatherModel(
+      {required this.cityName,
+      required this.date,
+      required this.image,
+      required this.maxTemp,
+      required this.minTemp,
+      required this.temp,
+      required this.weatherCondition});
+
+  factory WeatherModel.fromJson(json){
+    return  WeatherModel(cityName: cityName, date: date, image: image, maxTemp: maxTemp, minTemp: minTemp, temp: temp, weatherCondition: weatherCondition) 
+  }
 
 }
