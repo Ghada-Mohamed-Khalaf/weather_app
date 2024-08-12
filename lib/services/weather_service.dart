fbf000b842364f95bfd184eea710422a98f043f9
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-12 17:44:37 +0300

    dio exception

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -4,24 +4,29 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:weather_app/models/weather_model.dart';
 
-class WeatherServices{
-     final Dio dio ;
-     final String apiKey= "68e2c2078b044de3b3672943241108";
-      final String baseUrl='https://api.weatherapi.com';
+class WeatherServices {
+  final Dio dio;
 
-     WeatherServices(this.dio);
+  final String apiKey = "68e2c2078b044de3b3672943241108";
+  final String baseUrl = 'https://api.weatherapi.com';
 
-     Future< WeatherModel?> getCurrentWeather({required String cityName}) async{
+  WeatherServices(this.dio);
 
-      Response response=await dio.get('$baseUrl/v1/forecast.json?key=$apiKey&q=$cityName&days=1');
-      if(response.statusCode==201){
-      WeatherModel weatherModel=WeatherModel.fromJson(response.data);}
-      else{
-        final String errorMessage = response.data['error']['message'];
-        throw Exception(errorMessage);
+  Future<WeatherModel?> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio.get(
+          '$baseUrl/v1/forecast.json?key=$apiKey&q=$cityName&days=1');
+
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+    }
+    on  DioException catch(e){
+
+    final String errorMessage = e.response?.data['error']['message'] ?? 'oops there was an error,try again';
+   throw Exception(errorMessage);
+  }
+}
       }
 
-    }
-    }
+
 
 
