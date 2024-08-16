49cfecf329069f556c7fd008db6191c892d6d39b
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-16 23:41:43 +0300

    add row and  image of network

--- lib/widgets/weather_info_body.dart
+++ lib/widgets/weather_info_body.dart
@@ -5,6 +5,7 @@ import 'package:weather_app/views/search_view.dart';
 class WeatherInfoBody extends StatelessWidget {
   const WeatherInfoBody({super.key,required this.weatherModel});
     final WeatherModel  weatherModel;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -12,6 +13,10 @@ class WeatherInfoBody extends StatelessWidget {
       child:  Column(
         children: [
           Text(weatherModel.cityName,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+          Text("update at ${weatherModel.date}:${weatherModel.date}",style: TextStyle(fontSize: 24),),SizedBox(height: 32,),
+          Row(
+            children: [Image.network("https:${weatherModel.image}",height: 90,width: 90,),],
+          ),
 
           
 
