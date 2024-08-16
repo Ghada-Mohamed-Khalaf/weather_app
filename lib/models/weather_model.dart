c363e52b69ebf985d1b797ad9fb6ecdfcbebbc55
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-16 20:13:04 +0300

    add Bloc builder

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -16,8 +16,16 @@ class WeatherModel {
       required this.temp,
       required this.weatherCondition});
 
-  factory WeatherModel.fromJson(json){
-    return  WeatherModel(cityName:json["location"]['name'], date:json["current"]['last_updated'] , image: json['forecast']['forecastday'][0]['day']['condition'][ "icon"], maxTemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'] , minTemp: json['forecast']['forecastday'][0]['day']['mintemp_c'], temp: json['forecast']['forecastday'][0]["avgtemp_c"], weatherCondition: json['forecast']['forecastday'][0]['day']['condition']['text'],);
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+      cityName: json["location"]['name'],
+      date: json["current"]['last_updated'],
+      image: json['forecast']['forecastday'][0]['day']['condition']["icon"],
+      maxTemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+      minTemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+      temp: json['forecast']['forecastday'][0]["avgtemp_c"],
+      weatherCondition: json['forecast']['forecastday'][0]['day']['condition']
+          ['text'],
+    );
   }
-
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/views/search_view.dart';
 import 'package:weather_app/widgets/no_weather_body.dart';
@@ -27,8 +30,13 @@ class _HomeViewState extends State<HomeView> {
         ],
 
       ),
-      body:
-          NoWeatherBody(),
+      body:BlocBuilder<GetWeatherCubit,WeatherState>(
+        builder: (context,state)
+        {
+        return  NoWeatherBody();
+        },
+      )
+
 
 
     );
