2f06227681c3774351ee5dc6830969b032533535
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-16 23:17:27 +0300

    update in ui

--- lib/widgets/weather_info_body.dart
+++ lib/widgets/weather_info_body.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/views/search_view.dart';
 
 class WeatherInfoBody extends StatelessWidget {
-  const WeatherInfoBody({super.key});
-
+  const WeatherInfoBody({super.key,required this.weatherModel});
+    final WeatherModel  weatherModel;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: const Column(
-        children: [Text("vgdjdmdcndj")],
+      padding:  EdgeInsets.all(8.0),
+      child:  Column(
+        children: [
+          Text(weatherModel.cityName,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+
+          
+
+       ],
       ),
     );
   }
